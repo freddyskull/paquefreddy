147d4cb478c4492401491f39334c95d8e0ef3ecd
freddyskull <freddyskull11@gmail.com>
2025-03-30 14:43:45 -0400

    ✨ feat: agregar animaciones de entrada y salida a la tabla de datos utilizando react-transition-group que no funcionaron

--- src/components/dataTable/dataTable.jsx
+++ src/components/dataTable/dataTable.jsx
@@ -3,6 +3,7 @@ import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Button } from '../ui/button'
 import { Edit2, SearchCode, Trash } from 'lucide-react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export const DataTable = ({ columns, data, filtersTable, editItem, deleteItem }) => {
   const table = useReactTable({
@@ -23,28 +24,38 @@ export const DataTable = ({ columns, data, filtersTable, editItem, deleteItem })
 
   return (
     <ScrollArea className='my-2 mr-1 p-4 h-[80vh]'>
-      <div className='gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 pt-4 pb-4 w-full'>
+      <TransitionGroup className='gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 pt-4 pb-4 w-full'>
         {table.getRowModel().rows.map(row => (
-          <div key={row.id} className='relative p-4 product-container'>
-            {row.getVisibleCells().map(cell => (
-              <div key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+          <CSSTransition
+            key={row.id}
+            timeout={500} // Duración de la animación
+            classNames='fade' // Referencia a las clases CSS
+          >
+            <div className='relative p-4 product-container'>
+              {row.getVisibleCells().map(cell => (
+                <div key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              ))}
+              <div className='mt-4 actions'>
+                <Button variant='outline' className='px-2 h-8' onClick={() => editItem(row.original)}>
+                  <Edit2 />
+                </Button>
+                <Button
+                  variant='outline'
+                  className='hover:bg-destructive px-2 h-8'
+                  onClick={() => deleteItem(row.original)}
+                >
+                  <Trash />
+                </Button>
+                <Button variant='outline' className='hover:bg-warn px-2 h-8'>
+                  <SearchCode />
+                </Button>
               </div>
-            ))}
-            <div className='mt-4 actions'>
-              <Button variant='outline' className='px-2 h-8' onClick={() => editItem(row.original)}>
-                <Edit2 />
-              </Button>
-              <Button variant='outline' className='hover:bg-destructive px-2 h-8' onClick={() => deleteItem(row.original)}>
-                <Trash />
-              </Button>
-              <Button variant='outline' className='hover:bg-warn px-2 h-8'>
-                <SearchCode />
-              </Button>
             </div>
-          </div>
+          </CSSTransition>
         ))}
-      </div>
+      </TransitionGroup>
     </ScrollArea>
   )
 }

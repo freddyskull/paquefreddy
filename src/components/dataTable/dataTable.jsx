d1c3f560ff7d416e11ae2db774039fe3f5ad0df8
freddyskull <freddyskull11@gmail.com>
2025-03-31 07:07:14 -0400

    ♻️ refactor: eliminar referencias dinámicas en la tabla de datos y simplificar la estructura del componente

--- src/components/dataTable/dataTable.jsx
+++ src/components/dataTable/dataTable.jsx
@@ -1,9 +1,8 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Button } from '../ui/button'
 import { Edit2, SearchCode, Trash } from 'lucide-react'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export const DataTable = ({ columns, data, filtersTable, editItem, deleteItem }) => {
   const table = useReactTable({
@@ -22,55 +21,30 @@ export const DataTable = ({ columns, data, filtersTable, editItem, deleteItem })
     }
   })
 
-  // Mueve todos los refs a un objeto para evitar crearlos dinámicamente
-  const nodeRefs = useRef({})
-
   return (
     <ScrollArea className='my-2 mr-1 p-4 h-[80vh]'>
-      <TransitionGroup className='gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 pt-4 pb-4 w-full'>
-        {table.getRowModel().rows.map(row => {
-          // Asegúrate de que cada fila tenga un ref único
-          if (!nodeRefs.current[row.id]) {
-            nodeRefs.current[row.id] = React.createRef()
-          }
-
-          return (
-            <CSSTransition
-              key={row.id}
-              timeout={500} // Duración de la animación
-              classNames='fade' // Referencia a las clases CSS
-              nodeRef={nodeRefs.current[row.id]} // Usa el ref único para esta fila
-            >
-              <div ref={nodeRefs.current[row.id]} className='relative p-4 product-container'>
-                {row.getVisibleCells().map(cell => (
-                  <div key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </div>
-                ))}
-                <div className='mt-4 actions'>
-                  <Button
-                    variant='outline'
-                    className='px-2 h-8'
-                    onClick={() => editItem(row.original)}
-                  >
-                    <Edit2 />
-                  </Button>
-                  <Button
-                    variant='outline'
-                    className='hover:bg-destructive px-2 h-8'
-                    onClick={() => deleteItem(row.original)}
-                  >
-                    <Trash />
-                  </Button>
-                  <Button variant='outline' className='hover:bg-warn px-2 h-8'>
-                    <SearchCode />
-                  </Button>
-                </div>
+      <div className='gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 pt-4 pb-4 w-full'>
+        {table.getRowModel().rows.map(row => (
+          <div key={row.id} className='relative p-4 product-container'>
+            {row.getVisibleCells().map(cell => (
+              <div key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </div>
-            </CSSTransition>
-          )
-        })}
-      </TransitionGroup>
+            ))}
+            <div className='mt-4 actions'>
+              <Button variant='outline' className='px-2 h-8' onClick={() => editItem(row.original)}>
+                <Edit2 />
+              </Button>
+              <Button variant='outline' className='hover:bg-destructive px-2 h-8' onClick={() => deleteItem(row.original)}>
+                <Trash />
+              </Button>
+              <Button variant='outline' className='hover:bg-warn px-2 h-8'>
+                <SearchCode />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </ScrollArea>
   )
 }

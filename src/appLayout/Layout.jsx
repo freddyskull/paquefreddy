5bcbabeb6ad76b0e078a8c4926bc3c3211517310
freddyskull <freddyskull11@gmail.com>
2025-04-02 04:49:16 -0400

    💄 ui cambiar el tamaño por defecto del contenedor card

--- src/appLayout/Layout.jsx
+++ src/appLayout/Layout.jsx
@@ -5,6 +5,7 @@ import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/s
 import { SidebarComponent } from './components/sidebar'
 import { Card } from '@/components/ui/card'
 import { ScrollArea } from '@radix-ui/react-scroll-area'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from '@/components/ui/breadcrumb'
 
 export const Layout = ({ children, variant = 'card' }) => {
   // const [sidebarState, setsidebarState] = useState(localStorage.getItem('sidebarState'))
@@ -24,7 +25,7 @@ export const Layout = ({ children, variant = 'card' }) => {
           <div className='flex items-center gap-2 px-4'>
             <SidebarTrigger className='-ml-1' />
             {/* <Separator orientation="vertical" className="bg-slate-400 mr-2 h-4" /> */}
-            {/* <Breadcrumb>
+            <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className='hidden md:block'>
                   <BreadcrumbLink href='#'>
@@ -32,14 +33,14 @@ export const Layout = ({ children, variant = 'card' }) => {
                   </BreadcrumbLink>
                 </BreadcrumbItem>
               </BreadcrumbList>
-            </Breadcrumb> */}
+            </Breadcrumb>
           </div>
         </header>
         <div className='px-3'>
           {
             variant === 'card'
               ? (
-                <Card className='bg-slate-100 m-0 p-0 max-h-[92vh]'>
+                <Card className='bg-slate-100 m-0 p-0 h-[91vh]'>
                   <ScrollArea className='max-h-[94vh]'>
                     {children}
                   </ScrollArea>

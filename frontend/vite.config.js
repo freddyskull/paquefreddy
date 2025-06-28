d5320f3e81b481b1ff7ba00121e404385767d212
freddyskull <freddyskull11@gmail.com>
2025-06-28 11:45:39 -0400

    agregando el server a vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['paquefreddy.ddns.net'],
+    host: '0.0.0.0', // muy importante para aceptar conexiones externas
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
